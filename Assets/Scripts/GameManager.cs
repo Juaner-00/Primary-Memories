f5959ac5d30d8a420e1ce565cd61450dbc0a6809
Juaner-00 <juan.ramirezp@upb.edu.co>
2021-01-30 19:03:57 -0500

    Cambio de cursor
    se cambia el ícono del cursor dependiendo del objeto que se está seleccionado con el mouse

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Texture2D cursorNormal;
+
+    public Texture2D CursorNormal => cursorNormal;
+
+    static GameManager instance;
+    public static GameManager Instance => instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        instance = this;
+    }
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorNormal, Vector2.zero, CursorMode.Auto);
+    }
+
+}
