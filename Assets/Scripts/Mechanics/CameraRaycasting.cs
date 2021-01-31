28d1309da46f623158e80528a9378b9f66a7f7b6
Dagane <dagane8@hotmail.com>
2021-01-31 12:02:36 -0500

    Revert "Merge branch 'main' into Negrette"
    This reverts commit b740ab7d83db4393135e5aa5eb82d17cc49ab535.

--- Assets/Scripts/Mechanics/CameraRaycasting.cs
+++ Assets/Scripts/Mechanics/CameraRaycasting.cs
@@ -30,13 +30,9 @@ public class CameraRaycasting : MonoBehaviour
         {
             if (currentTarget != null)
             {
-                /* if (currentTarget.CanInteract())
-                { */
                 currentTarget.OnInteract(point);
-                // }
             }
         }
-
     }
 
     private void RaycastForInteractable()
@@ -45,15 +41,12 @@ public class CameraRaycasting : MonoBehaviour
 
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-
         if (Physics.Raycast(ray, out hit, range))
         {
             IInterectable interactable = hit.collider.GetComponent<IInterectable>();
 
             if (interactable != null)
             {
-                Debug.DrawLine(ray.origin, hit.point, Color.red, 0.1f);
-
                 point = hit.point;
 
                 if (hit.distance <= range)
@@ -66,19 +59,13 @@ public class CameraRaycasting : MonoBehaviour
                     {
                         currentTarget.OnEndHover();
                         currentTarget = interactable;
-                        /* if (currentTarget.CanInteract())
-                        { */
                         currentTarget.OnStartHover();
-                        // }
                         return;
                     }
                     else
                     {
                         currentTarget = interactable;
-                        /* if (currentTarget.CanInteract())
-                        { */
                         currentTarget.OnStartHover();
-                        // }
                         return;
                     }
                 }
@@ -94,8 +81,6 @@ public class CameraRaycasting : MonoBehaviour
             }
             else
             {
-                Debug.DrawLine(ray.origin, ray.direction * range, Color.white, 0.1f);
-
                 if (currentTarget != null)
                 {
                     currentTarget.OnEndHover();
