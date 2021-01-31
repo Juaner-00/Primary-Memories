026e59898cd8b159f8829b9b81660be1806c8b99
Juaner-00 <juan.ramirezp@upb.edu.co>
2021-01-31 17:11:45 -0500

    Revert "Revert "MECANICAS""
    This reverts commit fa91b6a2d9eea918b08177bd917e86ce4ac483ba.

--- Assets/Scripts/Mechanics/Interactables/Cuerdas1.cs
+++ Assets/Scripts/Mechanics/Interactables/Cuerdas1.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : InteractableBase
+public class Cuerdas1 : InteractableBase
 {
+    
     [SerializeField] float maxRange;
     [SerializeField] Texture2D cursorHover;
+    [SerializeField]
+    GameObject Puente, cuerda1,cuerda2,cuerda3,cuerda4;
 
     [SerializeField] string textPopUp;
+    [SerializeField] float tiempo;
 
 
     public override void Start()
@@ -20,12 +24,20 @@ public class Ball : InteractableBase
     {
         if (Vector3.Distance(transform.position, GameManager.Player.transform.position) <= maxRange)
         {
-            PopUpSystem.Instance.PopUpText(textPopUp, transform.position + Vector3.up * 0.8f);
             
+            PopUpSystem.Instance.PopUpText(textPopUp, transform.position + Vector3.up * 0.8f);
+            cuerda1.GetComponent<Rigidbody>().useGravity = true;
+            cuerda1.GetComponent<Rigidbody>().isKinematic = false;
+            cuerda2.GetComponent<Rigidbody>().useGravity = true;
+            cuerda2.GetComponent<Rigidbody>().isKinematic = false;
+
         }
         else
         {
             PopUpSystem.Instance.PopUpThinking("Can't reach that", GameManager.Player.transform.position + Vector3.up * 3);
         }
     }
+
+
+
 }
--- Assets/Scripts/Mechanics/Interactables/Cuerdas2.cs
+++ Assets/Scripts/Mechanics/Interactables/Cuerdas2.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : InteractableBase
+public class Cuerdas2 : InteractableBase
 {
+
     [SerializeField] float maxRange;
     [SerializeField] Texture2D cursorHover;
+    [SerializeField]
+    GameObject Puente, cuerda1, cuerda2, cuerda3, cuerda4;
 
     [SerializeField] string textPopUp;
+
 
 
     public override void Start()
@@ -20,12 +24,20 @@ public class Ball : InteractableBase
     {
         if (Vector3.Distance(transform.position, GameManager.Player.transform.position) <= maxRange)
         {
+
             PopUpSystem.Instance.PopUpText(textPopUp, transform.position + Vector3.up * 0.8f);
-            
+            cuerda3.GetComponent<Rigidbody>().useGravity = true;
+            cuerda3.GetComponent<Rigidbody>().isKinematic = false;
+            cuerda4.GetComponent<Rigidbody>().useGravity = true;
+            cuerda4.GetComponent<Rigidbody>().isKinematic = false;
+
         }
         else
         {
             PopUpSystem.Instance.PopUpThinking("Can't reach that", GameManager.Player.transform.position + Vector3.up * 3);
         }
     }
+
+
+
 }
